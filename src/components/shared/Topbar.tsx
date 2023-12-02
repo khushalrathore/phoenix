70094063229bd1dbd97dfe34ff840fbcf873b5e5
khushalrathore <118611135+khushalrathore@users.noreply.github.com>
2023-12-02 18:55:24 +0530

    Initial commit

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -24,6 +24,7 @@ const Greeting = () => {
 
   return <p className="greeting">Good {timeOfDay}</p>;
 };
+// ... (previous imports)
 
 const Topbar = () => {
   const navigate = useNavigate();
@@ -46,6 +47,10 @@ const Topbar = () => {
     if (isSuccess) navigate(0);
   }, [isSuccess]);
 
+  const menuItems = [
+    { title: "Log Out", iconPath: "public/assets/icons/logOut.png", onClick: signOut },
+    { title: "Settings", iconPath: "public/assets/icons/settings.png"},
+  ];
   return (
     <section className="topbar">
       <div className="flex-between pt-2 px-5">
@@ -61,7 +66,7 @@ const Topbar = () => {
           >
             <img 
               src={menuIcon} 
-              style={{ width: "18px", height: "18px" }} 
+              style={{ width: "35px", height: "35px" }} 
             />
           </Button>
 
@@ -69,19 +74,19 @@ const Topbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 bg-white absolute border border-stone-200 top-8 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            } bg-white absolute border border-stone-200 top-8 right-0 mx-4 my-2 min-w-[100px] h-[90px] pl-5 pb-1 rounded-xl sidebar`}
           >
             <ul className="list-none flex justify-end items-start flex-1 flex-col">
-              {/* Menu links with styling from the first list */}
-              {[
-                { title: "Log Out", onClick: () => { signOut(); setToggle(false); } },
-                { title: "Settings", onClick: () => { navigate('/saved'); setToggle(false); } },
-              ].map((nav, index) => (
+              {menuItems.map((nav, index) => (
                 <li
                   key={index}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] text-black mb-4`}
-                  onClick={nav.onClick}
+                  className={`font-poppins font-normal cursor-pointer text-[10px] text-black mb-4 flex items-center`}
                 >
+                  <img
+                    src={nav.iconPath}
+                    alt={`${nav.title} Icon`}
+                    style={{ width: "12px", height: "12px", marginRight: "8px"}}
+                  />
                   {nav.title}
                 </li>
               ))}
