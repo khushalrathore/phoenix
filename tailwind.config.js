74e6dc6a34d517740013e30a3aca08c3ac4fd71d
khushalrathore <118611135+khushalrathore@users.noreply.github.com>
2023-12-02 12:27:32 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,64 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
 
-module.exports = {
-  darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      
-      },
-    },
-    extend: {
-      colors: {
-        'primary-500': '#877EFF',
-        'primary-600': '#5D5FEF',
-        'secondary-500': '#FFB620',
-        'off-white': '#D0DFFF',
-        'red': '#FF5A5A',
-        'dark-1': '#000000',
-        'dark-2': '#09090A',
-        'dark-3': '#101012',
-        'dark-4': '#1F1F22',
-        'light-1': '#FFFFFF',
-        'light-2': '#EFEFEF',
-        'light-3': '#7878A3',
-        'light-4': '#5C5C7B',
-      },
-      screens: {
-        'xs': '480px',
-      
-      },
-      width: {
-        '420': '420px',
-        '465': '465px',
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+export const darkMode = ['class'];
+export const content = [
+  './pages/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+  './src/**/*.{ts,tsx}',
+];
+export const theme = {
+  container: {
+    center: true,
+    padding: '2rem',
+    screens: {
+      '2xl': '1400px',
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  extend: {
+    colors: {
+      'primary-500': '#FF8C00',   // Dark Orange
+      'primary-600': '#FFA500',   // Orange
+      'secondary-500': '#FFD700', // Gold
+      'off-white': '#FFE4B5',     // Moccasin
+      'red': '#FF4500',           // Orange Red
+      'dark-1': '#000000',        // Black
+      'dark-2': '#09090A',        // Charcoal
+      'dark-3': '#101012',        // Dark Charcoal
+      'dark-4': '#1F1F22',        // Very Dark Charcoal
+      'light-1': '#FFFFFF',       // White
+      'light-2': '#f7f3f2',       // Near White
+      'light-3': '#7878A3',       // Light Slate Gray
+      'light-4': '#5C5C7B',       // Slate Gray
+      'alphaDark' : 'rgba(0, 0, 0, 0.9)',
+      'alphaDark2' : 'rgba(255,255,255, 0.1)' 
+    },
+    
+    
+    screens: {
+      'xs': '480px',
+    },
+    width: {
+      '420': '420px',
+      '465': '465px',
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: 0 },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: 0 },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+    },
+  },
 };
+export const plugins = [require('tailwindcss-animate')];
